b930774651764d2ef99ebd9583c364a3051822d1
Mark Keinhoerster <mark.keinhoerster@gmail.com>
2015-06-09 00:56:09 +0200

    split tests

--- src/test/scala/com/netcloud/perceptron/EdgeTestSpec.scala
+++ src/test/scala/com/netcloud/perceptron/EdgeTestSpec.scala
@@ -11,53 +11,5 @@ class EdgeTestSpec extends WordSpec {
         edge.push((1))
       }
     }
-
-    "A perceptron" when {
-      "correctly wired up" should {
-        "properly recognize an AND as TRUE" in {
-          val statEdge = WiringEdge(-1.5)
-          val edge1 = WiringEdge(1)
-          val edge2 = WiringEdge(1)
-          val ins = List[InputEdge](statEdge, edge1, edge2)
-
-          val out = WiringEdge()
-          out.listen { tuple =>
-            tuple match {
-              case (activation, weight) => assert(activation >= (0.5))
-            }
-          }
-          val outs = List[OutputEdge](out)
-
-          val p = Perceptron(ins, outs)
-          statEdge.push(1)
-          edge1.push(1)
-          edge2.push(1)
-        }
-      }
-    }
-    
-    "A perceptron" when {
-      "correctly wired up" should {
-        "properly recognize an AND as FALSE" in {
-          val statEdge = WiringEdge(-1.5)
-          val edge1 = WiringEdge(1)
-          val edge2 = WiringEdge(1)
-          val ins = List[InputEdge](statEdge, edge1, edge2)
-
-          val out = WiringEdge()
-          out.listen { tuple =>
-            tuple match {
-              case (activation, weight) => assert(activation < (0.5))
-            }
-          }
-          val outs = List[OutputEdge](out)
-
-          val p = Perceptron(ins, outs)
-          statEdge.push(1)
-          edge1.push(0)
-          edge2.push(1)
-        }
-      }
-    }
   }
 }
